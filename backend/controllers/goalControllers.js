1b61487b0223c9dab7568a0807d56403c7a4dcf1
thehamzajunaid <junaidhamza4@gmail.com>
2022-05-31 01:05:30 +0500

    made rest api for crud

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,21 +1,19 @@
 const asyncHandler = require('express-async-handler');
+const Goal = require('../models/goalModel')
 
 
 // @desc Get goals
 // @routes GET /api/goals
 // @access Private
-
 const getGoals = asyncHandler(async (req, res) => {
+    const goals = await Goal.find()
     
-    res.status(200).json({
-        message: "Get goals"
-    })
+    res.status(200).json(goals)
 })
 
 // @desc Set goals
 // @routes SET /api/goals
 // @access Private
-
 const setGoal = asyncHandler(async (req, res) => {
     if (!req.body.text){
         res.status(400);
@@ -26,30 +24,47 @@ const setGoal = asyncHandler(async (req, res) => {
         // })
     };
 
-    res.status(200).json({
-        message: "Set goal"
+    const goal = await Goal.create({
+        text: req.body.text
     })
+
+    res.status(200).json(goal)
 })
 
 // @desc update goals
 // @routes PUT /api/goals/:id
 // @access Private
-
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: `Update goal ${req.params.id}`
-    })
+
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error("No goal of this ID found")
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    res.status(200).json(updatedGoal)
 })
 
 // @desc Delete goals
 // @routes DELETE /api/goals/:id
 // @access Private
-
 const deleteGoal = asyncHandler(async (req, res) => {
 
-    res.status(200).json({
-        message: `Delete goal ${req.params.id}`
-    })
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error("No goal of this ID found")
+    }
+
+    const deletedGoal = await Goal.findByIdAndDelete(req.params.id)
+
+    // or 
+    // await goal.remove()
+
+    res.status(200).json({id: deletedGoal.id})
 })
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const {errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000;
+const colors = require('colors');
+const {connectDB} = require('./config/db')
 
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
